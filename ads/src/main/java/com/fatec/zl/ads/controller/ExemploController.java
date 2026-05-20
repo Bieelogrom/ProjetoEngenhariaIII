37402b48d7004a7e023dfd6a1969662fa290228f
Bieelogrom <gabrielamorim15436@gmail.com>
2026-05-20 08:29:19 -0300

    init: commit inicial

---
+++ ads/src/main/java/com/fatec/zl/ads/controller/ExemploController.java
@@ -0,0 +1,26 @@
+package com.fatec.zl.ads.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/*
+
+Controller de exemplo
+
+*/
+
+
+@Controller
+@RequestMapping("/exemplo")
+public class ExemploController {
+    @GetMapping("/teste")
+    public String testarThymeleaf(@RequestParam("valor") String valor, Model model) {
+        model.addAttribute("valor", valor);
+        return "teste.html";
+    }
+    
+}
