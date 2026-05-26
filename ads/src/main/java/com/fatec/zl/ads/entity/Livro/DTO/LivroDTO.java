44fe7113dec55e268e26f0cdf8b4fb3957e3c841
Bieelogrom <gabrielamorim15436@gmail.com>
2026-05-25 22:06:29 -0300

    fix: correção de dto e factory para livro

--- ads/src/main/java/com/fatec/zl/ads/entity/Livro/DTO/LivroDTO.java
+++ ads/src/main/java/com/fatec/zl/ads/entity/Livro/DTO/LivroDTO.java
@@ -1,5 +1,7 @@
 package com.fatec.zl.ads.entity.Livro.DTO;
 
-public record LivroDTO(String titulo, Float preco, String categoria, String resumo, Integer paginas, String dataPublicacao, String isbn, String status, Integer quantidade, String formato) {
+import java.util.List;
+
+public record LivroDTO(String titulo, Float preco, String categoria, String resumo, Integer paginas, String dataPublicacao, String isbn, String status, Integer quantidade, String formato, List<String> listaDeAutores, String editora) {
 
 }
--- ads/src/main/java/com/fatec/zl/ads/entity/Livro/Factory/LivroFactory.java
+++ ads/src/main/java/com/fatec/zl/ads/entity/Livro/Factory/LivroFactory.java
@@ -3,9 +3,15 @@ package com.fatec.zl.ads.entity.Livro.Factory;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.fatec.zl.ads.entity.Autor.Autor;
+import com.fatec.zl.ads.entity.Autor.Exception.AutorNaoEncontradoException;
+import com.fatec.zl.ads.entity.Editora.Editora;
+import com.fatec.zl.ads.entity.Editora.Exception.EditoraNaoEncontradaException;
 import com.fatec.zl.ads.entity.Livro.Livro;
 import com.fatec.zl.ads.entity.Livro.LivroBrochura;
 import com.fatec.zl.ads.entity.Livro.LivroCapaDura;
@@ -14,9 +20,19 @@ import com.fatec.zl.ads.entity.Livro.LivroFisico;
 import com.fatec.zl.ads.entity.Livro.StatusLivro;
 import com.fatec.zl.ads.entity.Livro.DTO.LivroDTO;
 import com.fatec.zl.ads.entity.Livro.Exception.FormatoNaoReconhecidoException;
+import com.fatec.zl.ads.repository.AutorRepository;
+import com.fatec.zl.ads.repository.EditoraRepository;
 
 @Component
 public class LivroFactory {
+    private final AutorRepository autorRepository;
+    private final EditoraRepository editoraRepository;
+
+    public LivroFactory(AutorRepository autorRepository, EditoraRepository editoraRepository) {
+        this.autorRepository = autorRepository;
+        this.editoraRepository = editoraRepository;
+    }
+
     public Livro fabricarLivro(LivroDTO dto){
         Livro livro;
 
@@ -43,11 +59,28 @@ public class LivroFactory {
         livro.setDataPublicacao(formatarData(dto.dataPublicacao()));
         livro.setIsbn(dto.isbn());
         livro.setStatus(StatusLivro.valueOf(dto.status()));
+        livro.setAutores(buscarAutores(dto.listaDeAutores()));
+        livro.setEditora(buscarEditora(dto.editora()));
 
         return livro;
     }
 
-    private LocalDate formatarData(String data){
+    private Editora buscarEditora(String id){
+        return editoraRepository.findById(id).orElseThrow(() -> new EditoraNaoEncontradaException("Editora não encontrada!"));
+    }
+
+    private List<Autor> buscarAutores(List<String> ids){
+        List<Autor> listaDeAutores = new ArrayList<>();
+
+        for(String id : ids){
+            Autor autor = autorRepository.findById(id).orElseThrow(() -> new AutorNaoEncontradoException("Autor não encontrado!"));
+            listaDeAutores.add(autor);
+        }
+
+        return listaDeAutores;
+    }
+
+    private static LocalDate formatarData(String data){
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate dataConvertida = LocalDate.parse(data, formato);
         return dataConvertida;
