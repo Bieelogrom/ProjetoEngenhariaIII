952ccc6268413924ba72bf9cfac4147163bb6ae1
Bieelogrom <gabrielamorim15436@gmail.com>
2026-05-25 22:05:35 -0300

    feat: exceções personalizadas

---
+++ ads/src/main/java/com/fatec/zl/ads/entity/Autor/Exception/AutorNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.fatec.zl.ads.entity.Autor.Exception;
+
+public class AutorNaoEncontradoException extends RuntimeException {
+    public AutorNaoEncontradoException(String msg){
+        super(msg);
+    }
+}
---
+++ ads/src/main/java/com/fatec/zl/ads/entity/Editora/Exception/EditoraNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.fatec.zl.ads.entity.Editora.Exception;
+
+public class EditoraNaoEncontradaException extends RuntimeException {
+    public EditoraNaoEncontradaException(String msg){
+        super(msg);
+    }
+}
