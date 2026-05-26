0d8788221327b8be375f3ab23151af1d6ef2badb
Bieelogrom <gabrielamorim15436@gmail.com>
2026-05-25 21:19:21 -0300

    feat: repositorys e services

---
+++ ads/src/main/java/com/fatec/zl/ads/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.zl.ads.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.zl.ads.entity.Autor.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor, String> {
+
+}
---
+++ ads/src/main/java/com/fatec/zl/ads/repository/EditoraRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.zl.ads.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.zl.ads.entity.Editora.Editora;
+
+public interface EditoraRepository extends JpaRepository<Editora, String> {
+
+}
