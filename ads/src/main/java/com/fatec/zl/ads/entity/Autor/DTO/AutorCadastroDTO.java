5772a65913b720d334a082b69470f12d05c9a467
Bieelogrom <gabrielamorim15436@gmail.com>
2026-05-30 12:12:30 -0300

    feat: cadastrar autor

---
+++ ads/src/main/java/com/fatec/zl/ads/entity/Autor/DTO/AutorCadastroDTO.java
@@ -0,0 +1,5 @@
+package com.fatec.zl.ads.entity.Autor.DTO;
+
+public record AutorCadastroDTO(String nome, String dataNasc, String nacionalidade) {
+
+}
--- ads/src/main/java/com/fatec/zl/ads/service/AutorService.java
+++ ads/src/main/java/com/fatec/zl/ads/service/AutorService.java
@@ -2,7 +2,24 @@ package com.fatec.zl.ads.service;
 
 import org.springframework.stereotype.Service;
 
+import com.fatec.zl.ads.entity.Autor.Autor;
+import com.fatec.zl.ads.entity.Autor.DTO.AutorCadastroDTO;
+import com.fatec.zl.ads.infra.utils.DateUtils;
+import com.fatec.zl.ads.repository.AutorRepository;
+
 @Service
 public class AutorService {
+    private final AutorRepository autorRepository;
 
+    public AutorService(AutorRepository autorRepository){
+        this.autorRepository = autorRepository;
+    }
+
+    public Autor cadastrarAutor(AutorCadastroDTO dto){
+        Autor novoAutor = new Autor();
+        novoAutor.setNome(dto.nome());
+        novoAutor.setDataNasc(DateUtils.formatarData(dto.dataNasc()));
+        novoAutor.setNacionalidade(dto.nacionalidade());
+        return autorRepository.save(novoAutor);
+    }
 }
