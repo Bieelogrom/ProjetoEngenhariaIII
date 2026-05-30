1cd31581f2ef5b20a1257bb48af9933e09532692
Bieelogrom <gabrielamorim15436@gmail.com>
2026-05-30 12:20:24 -0300

    feat: cadastro da editora

---
+++ ads/src/main/java/com/fatec/zl/ads/entity/Editora/DTO/EditoraCadastroDTO.java
@@ -0,0 +1,5 @@
+package com.fatec.zl.ads.entity.Editora.DTO;
+
+public record EditoraCadastroDTO(String nome, String cnpj, String telefone, String email) {
+
+}
--- ads/src/main/java/com/fatec/zl/ads/service/EditoraService.java
+++ ads/src/main/java/com/fatec/zl/ads/service/EditoraService.java
@@ -2,7 +2,24 @@ package com.fatec.zl.ads.service;
 
 import org.springframework.stereotype.Service;
 
+import com.fatec.zl.ads.entity.Editora.Editora;
+import com.fatec.zl.ads.entity.Editora.DTO.EditoraCadastroDTO;
+import com.fatec.zl.ads.repository.EditoraRepository;
+
 @Service
 public class EditoraService {
-    
+    private final EditoraRepository editoraRepository;
+
+    public EditoraService(EditoraRepository editoraRepository){
+        this.editoraRepository = editoraRepository;
+    }
+
+    public Editora cadastrarEditora(EditoraCadastroDTO dto){
+        Editora novaEditora = new Editora();
+        novaEditora.setNome(dto.nome());
+        novaEditora.setEmail(dto.email());
+        novaEditora.setCnpj(dto.cnpj());
+        novaEditora.setTelefone(dto.telefone());
+        return editoraRepository.save(novaEditora);
+    }
 }
